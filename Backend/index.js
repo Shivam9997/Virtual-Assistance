a4468fc2ea6063807c67aadae9b2423a6ef373f0
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-17 22:22:56 +0530

    Add new Files

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv";
+dotenv.config()
+import connectDb from "./config/db.js";
+import cors from "cors"
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+
+
+
+
+const app= express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true
+}))
+const port=process.env.PORT || 8000
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+
+
+
+app.listen(port,()=>{
+    connectDb()
+    console.log(`localhost:${port}`)
+    console.log("Server Started")
+})
