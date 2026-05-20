ffe9f4e24cb0d808fdbe6ed70b314b405b21ee7d
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-20 18:57:14 +0530

    Add new Files

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ import connectDb from "./config/db.js";
 import cors from "cors"
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
 
 
 
@@ -19,6 +20,7 @@ const port=process.env.PORT || 8000
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
 
 
