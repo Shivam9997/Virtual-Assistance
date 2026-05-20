01f99cecdc9c8276fb3289759073e4cbf1131eaf
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-20 18:57:54 +0530

    Add new Files

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react-refresh/only-export-components */
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+import { createContext } from "react"
+
+export const UserDataContext = createContext()
+
+const UserContext = ({children}) => {
+    const serverUrl= "http://localhost:8000"
+    const [userData,setUserData]= useState(null)
+
+    const handleCurrentUser=async ()=>{
+        try {
+             const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
+             setUserData(result.data)
+             console.log(result.data)
+        } catch (error) {
+             console.log(error)
+             
+        }
+    }
+
+    useEffect(()=>{
+  handleCurrentUser()
+ },[])
+
+    const value={
+        serverUrl,
+           
+
+    }
+  return (
+    
+        <UserDataContext.Provider value={value}>
+        {children}
+        </UserDataContext.Provider>
+      
+    
+  )
+}
+
+export default UserContext
