56ba42ab99dea51afd055dc993ae8e7cc7e71084
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-21 13:55:46 +0530

    Updates Files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn"
 import Customize from "./pages/Customize"
+import { useContext } from "react"
+import { UserDataContext } from "./context/UserContext"
+import Home from "./pages/Home"
 
 
 const App = () => {
+  const {userData,setUserData}= useContext(UserDataContext)
   return (
    <Routes>
-    <Route path='/SignUp' element={<SignUp/>}/>
-    <Route path='/SignIn' element={<SignIn/>}/>
-    <Route path='/Customize' element={<Customize/>}/>
+
+    <Route path='/' element={(userData?.assistantImage && userData.assistantName)? <Home/>: <Navigate to={"/Customize"}/>}/>
+    <Route path='/signUp' element={!userData?<SignUp/>: <Navigate to={"/"}/>}/>
+    <Route path='/signIn' element={!userData?<SignIn/>: <Navigate to={"/"}/>}/>
+    <Route path='/customize' element={userData?<Customize/>:<Navigate to={"/SignIn"}/>}/>
 
    </Routes>
   )
