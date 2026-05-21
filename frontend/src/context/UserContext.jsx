501b7fdc1ab7043c6d232dcea07628d8625a1a29
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-21 13:55:57 +0530

    Updates Files

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -10,23 +10,23 @@ const UserContext = ({children}) => {
     const serverUrl= "http://localhost:8000"
     const [userData,setUserData]= useState(null)
 
-    const handleCurrentUser=async ()=>{
-        try {
-             const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
-             setUserData(result.data)
-             console.log(result.data)
-        } catch (error) {
-             console.log(error)
-             
-        }
-    }
 
     useEffect(()=>{
-  handleCurrentUser()
- },[])
+      const handleCurrentUser = async () => {
+        try {
+          const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true })
+          setUserData(result.data)
+          console.log(result.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      handleCurrentUser()
+    },[])
 
     const value={
-        serverUrl,
+        serverUrl,userData,setUserData
            
 
     }
