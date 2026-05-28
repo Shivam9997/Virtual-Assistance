315b108c03e77e3bcb7bdadd4e0268f255b3733b
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-29 01:15:25 +0530

    Add new Files

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { UserDataContext } from "../context/UserContext";
+
+const Card = ({ image }) => {
+  const {
+    serverUrl,
+    userData,
+    setUserData,
+    frontendImage,
+    setFrontendImage,
+    backendImage,
+    setBackendImage,
+    selectedImage,
+    setSelectedImage,
+  } = useContext(UserDataContext);
+  return (
+    <div
+      className= {`w-[70px] h-[130px] lg:w-[150px] lg:h-[250px] bg-[#07073c] border-2 border-[#0808386b] rounded-2xl overflow-hidden
+    hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-b-blue-500 ${selectedImage==image?"border-4 border-b-blue-500 shadow-2xl shadow-blue-950":null} `}
+      onClick={() => setSelectedImage(image)}
+    >
+      <img src={image} alt="card" className="h-full object-cover " />
+    </div>
+  );
+};
+
+export default Card;
