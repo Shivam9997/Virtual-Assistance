3dc27354bccafc4e16104f5367309ac4ec997709
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-30 01:11:30 +0530

    edit pages

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -17,7 +17,11 @@ const Card = ({ image }) => {
     <div
       className= {`w-[70px] h-[130px] lg:w-[150px] lg:h-[250px] bg-[#07073c] border-2 border-[#0808386b] rounded-2xl overflow-hidden
     hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-b-blue-500 ${selectedImage==image?"border-4 border-b-blue-500 shadow-2xl shadow-blue-950":null} `}
-      onClick={() => setSelectedImage(image)}
+      onClick={() => {
+        setSelectedImage(image)
+        setBackendImage(null)
+        setFrontendImage(null)
+      }}
     >
       <img src={image} alt="card" className="h-full object-cover " />
     </div>
