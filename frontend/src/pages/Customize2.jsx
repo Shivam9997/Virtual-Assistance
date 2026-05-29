7b35aeefe6e9ffa5e53c5ab4dea05bff05c07ed5
Shivam9997 <shivamrathore2805@gmail.com>
2026-05-30 01:11:36 +0530

    edit pages

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -1,6 +1,38 @@
+import { useContext, useState } from "react"
+import { UserDataContext } from "../context/UserContext"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
 
 
 const Customize2 = () => {
+    const {userData,backendImage,selectedImage,serverUrl,setUserData}= useContext(UserDataContext)
+    const [assistantName, setAssitantName]= useState(userData?.AssistantName || "")
+
+     const Navigate = useNavigate();
+
+    const handleUpdateAssistant=async()=>{
+        try {
+            let formData=new FormData()
+            formData.append("assistantName",assistantName)
+            if(backendImage){
+                formData.append("assistantImage", backendImage)
+            }
+            else{
+                formData.append("imageUrl", selectedImage)
+            }
+            const result = await axios.post(`${serverUrl}/api/user/update`,
+                formData,{withCredentials:true}
+            )
+            console.log(result.data)
+            setUserData(result.data)
+            
+        } catch (error) {
+            
+            console.log(error.response.data)
+        }
+
+    }
   return (
     <div  className="w-full h-screen bg-linear-to-t from-[#160202] to-[#020258f3] 
      flex justify-center items-center flex-col p-6 ">
@@ -28,9 +60,11 @@ const Customize2 = () => {
           focus:ring-2
           focus:ring-sky-400/40
         "
+        onChange={(e)=>setAssitantName(e.target.value)}
+        value={assistantName}
         required 
         />
-         <button
+        {assistantName &&  <button
           className="min-w-[250px] h-[60px] mt-[30px] text-black font-semibold text-[19px] rounded-full cursor-pointer  border
           border-white
           bg-lime-100
@@ -38,10 +72,16 @@ const Customize2 = () => {
           focus:border-b-rose-900
           focus:ring-2
           focus:ring-sky-400/40"
+          onClick={()=>{
+            Navigate("/customize2")
+            handleUpdateAssistant()
+        }}
          
         >
          Create Your Assistant
-        </button>
+        </button> 
+}
+        
     </div>
   )
 }
